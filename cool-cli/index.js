2b7eb307077ef3b6c647df10d38f50e7573f8077
Rajesh <rajeshdyawarkonda123@gmail.com>
2023-07-08 18:34:53 +0530

    added cool-cli

---
+++ cool-cli/index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const argument = process.argv[2];
+const fs = require("fs");
+
+if (!argument) {
+	console.log("cool works");
+}
+
+if (argument === "create-sample") {
+	fs.writeFile("sample.txt", "sample text", () => {
+		console.log("sample file is created");
+	});
+}
+
+if (argument === "create-react-component") {
+	const reactComponent = `import React from "react";
+
+	export default function Component(props) {
+		return <div>Component</div>;
+	}`;
+	fs.writeFile("component.jsx", reactComponent, () => {
+		console.log("react component is created");
+	});
+}
+
+if (argument === "random-color") {
+	const red = Math.floor(Math.random() * 255);
+	const green = Math.floor(Math.random() * 255);
+	const blue = Math.floor(Math.random() * 255);
+	console.log(`rgb(${red}, ${green}, ${blue})`);
+}
